c4a54008af3b22d8b668c45d9430f8214120bf93
Bernardo <bernardobustamantemartinez@gmail.com>
2023-07-19 11:59:55 -0700

    state init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+import { headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation } from '../config/motion';
+
+import state from '../store';
 
 const Home = () => {
+  const snap = useSnapshot(state);
   return (
     <div>Home</div>
   )
